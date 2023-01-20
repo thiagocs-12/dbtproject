0073587c1f2fb48fb65e28d61757480936a1a81e
Thiago da Costa Santos <thiago.dacosta.s@decolar.com>
2023-01-20 11:31:11 +0000

    partitions

--- models/employees.sql
+++ models/employees.sql
@@ -2,7 +2,7 @@ with calc_employees as (
     select 
     date_part(year, current_date) - date_part(year, birth_date) age,
     date_part(year, current_date) - date_part(year, hire_date) lengthofservice,
-    first_name || ' ' || last_name, *
+    first_name || ' ' || last_name name, *
     from {{source('sources', 'employees')}}
 )
 select * from calc_employees
